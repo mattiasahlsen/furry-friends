d697d7d0eb732d9380d86969bacfa4131029ad7d
Mattias Ahlsén <mattias.ahlsen@gmail.com>
2023-03-16 10:30:38 +0100

    Created Furry Friends Base

---
+++ src/data/cats.ts
@@ -0,0 +1,37 @@
+export const CATS = [
+  {
+    image:
+      'https://plus.unsplash.com/premium_photo-1661508614319-b5e40d1143bb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2F0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
+    name: 'Mittens',
+    description:
+      'Mittens is a 3 year old cat who loves to play with yarn and eat fish.',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2F0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
+    name: 'Fluffy',
+    description:
+      'Fluffy is a 5 year old cat who loves to play with yarn and eat fish.',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1533738363-b7f9aef128ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y2F0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
+    name: 'Patches',
+    description:
+      'Patches is a 2 year old cat who loves to play with yarn and eat fish.',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1573865526739-10659fec78a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Y2F0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
+    name: 'Tiger',
+    description:
+      'Tiger is a 1 year old cat who loves to play with yarn and eat fish.',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1495360010541-f48722b34f7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y2F0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
+    name: 'Socks',
+    description:
+      'Socks is a 4 year old cat who loves to play with yarn and eat fish.',
+  },
+]
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,5 +2,17 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="min-h-screen flex flex-col justify-between bg-yellow-400">
+      <header className="bg-yellow-100 text-slate-800 text-xl font-semibold">
+        <div className="container py-4 px-2 mx-auto">Furry Friends</div>
+      </header>
+      <Component {...pageProps} />
+      <footer className="bg-yellow-100 flex justify-center">
+        <p className="text-slate-800 py-2 font-semibold">
+          © 2023 Mattias Ahlsén
+        </p>
+      </footer>
+    </div>
+  )
 }
